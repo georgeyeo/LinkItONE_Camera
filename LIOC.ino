93bd7b4f1187ce071db22ea4b2626c0d9ef6d0cb
georgeyeo <linuxyeo@gmail.com>
2015-09-27 22:07:26 +0800

    Update LIOC.ino

--- LIOC.ino
+++ LIOC.ino
@@ -8,7 +8,7 @@
 	 ** CLK - pin 13
 	 ** CHIP_SELECT - pin 4
 
-	modified 25 September 2015
+	modified 27 September 2015
 	by George Yeo
 
 	Examples:
@@ -107,18 +107,22 @@ void setup() {
 
 	// print the type and size of the first FAT-type volume
 	uint32_t volumesize;
+	
 	Serial.print("\nVolume type is FAT");
 	Serial.println(volume.fatType(), DEC);
 	Serial.println();
 
-	volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-	volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-	volumesize *= 512;                            // SD card blocks are always 512 bytes
+	volumesize = volume.blocksPerCluster();    		// clusters are collections of blocks
+	volumesize *= volume.clusterCount();       		// we'll have a lot of clusters
+	volumesize *= 512;                            	// SD card blocks are always 512 bytes
+	
 	Serial.print("Volume size (bytes): ");
 	Serial.println(volumesize);
+	
 	Serial.print("Volume size (Kbytes): ");
 	volumesize /= 1024;
 	Serial.println(volumesize);
+	
 	Serial.print("Volume size (Mbytes): ");
 	volumesize /= 1024;
 	Serial.println(volumesize);
